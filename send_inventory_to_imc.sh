5c81dd063b104e25af7e2c688e56f05f76bb9dde
pnithyan <pnithyan@cisco.com>
2025-09-04 10:19:32 +0530

    Added support for mustang servers

--- send_inventory_to_imc.sh
+++ send_inventory_to_imc.sh
@@ -5,17 +5,22 @@ export PATH=$PATH:/sbin:/usr/sbin
 ipmitoolcmd=`which ipmitool`
 installationPath="/opt/ucs-tool"
 inventoryfilename=$installationPath/"host-inv.yaml"
+netfunction="0x36"
+servermodel=`/usr/sbin/dmidecode -s system-product-name`
+if [[ $servermodel == "CAI-845A-M8" ]]; then
+    netfunction="0x34"
+fi
 
 if [ $(stat -c %s $inventoryfilename) -ge 65535 ] ; then
 	echo Exiting! $file is over maximum size of 65535
 fi
 
 echo "[localhost]: Removing existing host-inv.yaml inventory file from IMC"
-cmd="${ipmitoolcmd} raw 0x36 0x77 0x03 0x68 0x6f 0x73 0x74 0x2d 0x69 0x6e 0x76 0x2e 0x79 0x61 0x6d 0x6c"
+cmd="${ipmitoolcmd} raw ${netfunction} 0x77 0x03 0x68 0x6f 0x73 0x74 0x2d 0x69 0x6e 0x76 0x2e 0x79 0x61 0x6d 0x6c"
 $cmd
 
 echo "[localhost]: Getting File Descriptor For host-inv.yaml inventory file from IMC"
-filedescriptor=$(${ipmitoolcmd} raw 0x36 0x77 0x00 0x68 0x6f 0x73 0x74 0x2d 0x69 0x6e 0x76 0x2e 0x79 0x61 0x6d 0x6c)
+filedescriptor=$(${ipmitoolcmd} raw ${netfunction} 0x77 0x00 0x68 0x6f 0x73 0x74 0x2d 0x69 0x6e 0x76 0x2e 0x79 0x61 0x6d 0x6c)
 filedescriptor="0x${filedescriptor:1}"
 
 filebytearray=($(od -An -vtx1 $inventoryfilename | tr -d '\n'))
@@ -36,7 +41,7 @@ for (( i=0; i<${#filebytearray[@]}; i++ )); do
         payload+=$(printf '0x%s ' "$b")
         filepointer=$(printf "0x%04X" $filelocationpointer)
         filepointer=$(echo $filepointer | sed -E 's/0x(..)(..)/0x\2 0x\1/')
-        cmd="${ipmitoolcmd} raw 0x36 0x77 0x02 "$filedescriptor" "$payloadlength" "$filepointer" 0x00 0x00 "$payload
+        cmd="${ipmitoolcmd} raw ${netfunction} 0x77 0x02 "$filedescriptor" "$payloadlength" "$filepointer" 0x00 0x00 "$payload
         filelocationpointer=$((filelocationpointer+40))
         $cmd
         counter=0
@@ -46,9 +51,9 @@ done
 
 filepointer=$(printf "0x%04X" $filelocationpointer)
 filepointer=$(echo $filepointer | sed -E 's/0x(..)(..)/0x\2 0x\1/')
-cmd="${ipmitoolcmd} raw 0x36 0x77 0x02 "$filedescriptor" "$(printf '0x%x\n' $counter)" "$filepointer" 0x00 0x00 "$payload
+cmd="${ipmitoolcmd} raw ${netfunction} 0x77 0x02 "$filedescriptor" "$(printf '0x%x\n' $counter)" "$filepointer" 0x00 0x00 "$payload
 $cmd
 
 echo "[localhost]: Closing IMC file handle"
-cmd="${ipmitoolcmd} raw 0x36 0x77 0x01 "$filedescriptor
+cmd="${ipmitoolcmd} raw ${netfunction} 0x77 0x01 "$filedescriptor
 $cmd
